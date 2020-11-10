feac650b368c9668c2d491c99371e17ecfd3c0f7
toyama1710 <walker2002226@gmail.com>
2020-11-11 00:26:47 +0900

    prot typing

---
+++ src/client/depcl.rs
@@ -0,0 +1,69 @@
+use std::io;
+use std::fs;
+use std::path::Path;
+use clap::{App, Arg};
+use std::fs::File;
+
+// return (section, hostname, port, keyfile, publish_dir)
+pub fn get_config() -> (String, String, u32, File, String) {
+    let args = App::new("ssg-deployer(client)")
+    .version(clap::crate_version!())
+    .author(clap::crate_authors!())
+    .about("simple deployer")
+    .args(
+        &[
+        Arg::from_usage("[section]<SECTION>"),
+        Arg::from_usage("[hostname] -d <HOST> 'assign destination'"),
+        Arg::from_usage("[port] -p --port <INTEGER> 'sets port number to send request'")
+            .validator(|s| {
+                let v = s.parse::<usize>();
+                match v {
+                    Ok(v) => {
+                        if 1024 <= v && v <= 49512 { return Ok(()); }
+                        else { return Err(String::from("port number must be 1024..=49512")); }
+                    },
+                    Err(_) => {
+                        return Err(String::from("-p value must be integer"));
+                    },
+                }
+        }),
+        Arg::from_usage("[identity file] -i --identity <FILE> 'sets identity file to use'")
+            .validator(|s| {
+                let p = Path::new(std::ffi::OsStr::new(&s));
+                if p.exists() && p.is_file() { Ok(()) }
+                else { Err(String::from("file not found")) }
+        }),
+        Arg::from_usage("[publish_dir] -s <PUBLISH_DIR> 'assign publish directory")
+             .validator(|s| {
+                let p = Path::new(std::ffi::OsStr::new(&s));
+                if p.exists() && p.is_dir() { Ok(()) }
+                else { Err(String::from("missing path")) }
+        }),
+        ]
+    )
+    .get_matches();
+
+    let section = String::from(args.value_of("section").unwrap());
+    let host = String::from(args.value_of("hostname").unwrap());
+    let port = args.value_of("port").unwrap().parse::<u32>().unwrap();
+    let path = args.value_of("identity file").unwrap().to_owned().to_string();
+    let key = File::open(path).unwrap();
+    let dir = String::from(args.value_of("publish_dir").unwrap());
+    return (section, host, port, key, dir);
+}
+
+pub fn visit_dir(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dir(&path)?;
+            }
+            else {
+                println!("{}", path.display());
+            }
+        }
+    }
+    Ok(())
+}
--- src/client/main.rs
+++ src/client/main.rs
@@ -1,80 +1,55 @@
-use std::io;
+use std::io::*;
 use std::fs;
 use std::path::Path;
-use clap::{App, Arg};
-use std::fs::File;
-
-// return (hostname, port, keyfile)
-pub fn get_config() -> (String, u32, File) {
-    let args = App::new("ssg-deployer(client)")
-    .version("0.0.0")
-    .author("toyama1710")
-    .about("simple deployer")
-    .arg(
-        Arg::with_name("hostname")
-             .short("d")
-             .help("assign destination")
-             .takes_value(true)
-             .value_name("host")
-    )
-    .arg(
-        Arg::with_name("identity file")
-            .short("i")
-            .long("identity")
-            .help("sets identity file to use")
-            .takes_value(true)
-            .value_name("FILE")
-            .validator(|s| {
-                let p = Path::new(std::ffi::OsStr::new(&s));
-                if p.exists() { Ok(()) }
-                else { Err(String::from("file not found")) }
-            })
-    )
-    .arg(
-        Arg::with_name("port")
-            .short("p")
-            .long("port")
-            .help("sets port number to send request")
-            .takes_value(true)
-            .value_name("INTEGER")
-            .validator(|s| {
-                let v = s.parse::<usize>();
-                match v {
-                    Ok(v) => {
-                        if 1024 <= v && v <= 49512 { return Ok(()); }
-                        else { return Err(String::from("port number must be 1024..=49512")); }
-                    },
-                    Err(_) => {
-                        return Err(String::from("-p value must be integer"));
-                    },
-                }
-            })
-    )
-    .get_matches();
-
-    let host = args.value_of("hostname").unwrap().to_owned().to_string();
-    let p = args.value_of("port").unwrap().parse::<u32>().unwrap();
-    let path = args.value_of("identity file").unwrap().to_owned().to_string();
-    let key = File::open(path).unwrap();
-    return (host, p, key);
-}
-
-fn visit_dir(dir: &Path) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dir(&path)?;
-            }
-            else {
-                println!("{}", path.display());
-            }
-        }
-    }
-    Ok(())
-}
+use std::net::*;
+mod depcl;
 
 fn main() {
-    let (host, port, mut key) = get_config();
+    let (section, host, port, mut key, dir) = depcl::get_config();
+    let dir = Path::new(&dir);
+    let addr = format!("{}:{}", host, port);
+    let addr = addr.to_socket_addrs();
+    if let Err(_) = addr { 
+        eprintln!("can't resolve host");
+        return;
+    }
+
+    let addr = addr.unwrap().find(|x| (*x).is_ipv4()).unwrap();
+    match TcpStream::connect(addr) {
+        Err(e) => {
+            eprintln!("{:?}", e);
+            return;
+        }
+        Ok(stream) => {
+            let mut reader = BufReader::new(&stream);
+            let mut writer = BufWriter::new(&stream);
+            let mut msg = String::new();
+
+            writer.write(section.as_bytes()).unwrap();
+
+            // auth
+            /*
+            */
+
+            // send_hashs()
+            /*
+                for entry in get_hashs() {
+                }
+            */
+
+            // send_files()
+            /*
+                loop {
+                    reader.read_line(&mut msg);
+                    if msg != "end." {
+                        send_file();
+                    }
+                }
+            */
+
+            /*
+                writer.write("all file sended\n")
+            */
+        }
+    }
 }
